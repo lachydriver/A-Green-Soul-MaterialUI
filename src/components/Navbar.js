64dd53063804ce2f2d39fe85681ffaca7fe38d83
lachydriver <lachydriver@gmail.com>
2020-05-13 13:53:47 +1000

    Updated Landing Page and Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { createMuiTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -25,7 +24,7 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" style={{marginBottom: '0px'}}>
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import logo from '../content/logo.png';
 
 class Landing extends React.Component {
-    render(){
-        return(
-            <div>
-                <p>LANDING PAGE</p>
-            </div>
+    render() {
+        return (
+            <Grid container direction="column">
+                <Grid item>
+                    <Typography variant='h2'>
+                        A Green Soul
+                    </Typography>
+                    <Typography variant='subtitle1'>
+                        Saving the planet. One home at a time.
+                    </Typography>
+                    <img src={logo}/>
+                </Grid>
+            </Grid>
         )
     }
 }
